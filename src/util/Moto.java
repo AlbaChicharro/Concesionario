5ad8d0185ed5c0614b8130256c756401368c2860
Alba Chicharro <a.chicharro.albacete@gmail.com>
2024-01-25 13:02:21 +0100

    Añado las motos

---
+++ src/util/Moto.java
@@ -0,0 +1,22 @@
+package util;
+
+public class Moto {
+
+	private String nombreMoto;
+	private String matricula;
+	private String marca;
+	private String modelo;
+	private int anio;
+	private int caballos;
+	
+	
+	public Moto(String nombreMoto, String matricula, String marca, String modelo, int anio, int caballos) {
+		this.nombreMoto = nombreMoto;
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.anio = anio;
+		this.caballos = caballos;
+	}
+	
+}
