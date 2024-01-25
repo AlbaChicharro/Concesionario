d52d303d57aa10d22cbd2f0b173b5b73ab5a6c25
lorena232323 <lorena23022002@gmail.com>
2024-01-25 12:42:12 +0100

    añadir clase coche otra vez

---
+++ src/util/coche.java
@@ -0,0 +1,19 @@
+package util;
+
+public class coche {
+	private String nombrecoche;
+	private String matricula;
+	private String marca;
+	private String modelo;
+	private int anio;
+	
+	
+	public coche(private String nombrecoche, String matricula, private String marca, private String modelo, int anio) {
+		this.nombrecoche = nombrecoche;
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.anio = anio;
+   
+	}
+}
