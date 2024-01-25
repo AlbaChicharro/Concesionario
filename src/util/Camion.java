26386a98bdf9bd45b468da35b3f0db77c06a6d29
romga24 <romgarro24@gmail.com>
2024-01-25 12:27:17 +0100

    Cambios

---
+++ src/util/Camion.java
@@ -0,0 +1,20 @@
+package util;
+
+public class Camion {
+
+	private String nombreCamion;
+	private String matricula;
+	private String marca;
+	private String modelo;
+	private int anio;
+	
+	
+	public Camion(private String nombreCamion, String matricula, private String marca, private String modelo, int anio) {
+		this.nombreCamion = nombreCamion;
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.anio = anio;
+	}
+	
+}
